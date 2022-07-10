9f77e8d10a2037f607bae2702f1cc2436ffc2806
Tarik Yayla <tarik.yayla@ouriginal.com>
2022-07-10 21:51:55 +0200

    pagination class added

--- api/actions.py
+++ api/actions.py
@@ -6,9 +6,31 @@ from flask_cors import CORS
 api = Blueprint("api", __name__) 
 CORS(api)
 
+# I didnt want to install pydantic for validation
+ # so thats why its pretty easy to break if you insist. 
+
+class QueryParams(object):
+  page = 0
+  items_per_page = 20
+  
+  def __init__(self, page, items_per_page):
+    self.page = page
+    self.items_per_page = items_per_page
+
+  @staticmethod
+  def get_from_args(args):
+    page = int(args.get("page", 0))
+    items_per_page = int(args.get("items-per-page", 20))
+
+    return QueryParams(page, items_per_page)
+
+
+# first time using peewee, looks pretty cool
+
 @api.route("/locations", methods=["GET"]) 
 def get_locations():
-  data = Locations.select().order_by(Locations.id).dicts()
+  params = QueryParams.get_from_args(request.args);
+  data = Locations.select().paginate(params.page, params.items_per_page).order_by(Locations.id).dicts()
   return jsonify(list(data))
 
 @api.route("locations/<id>", methods=["GET"])
