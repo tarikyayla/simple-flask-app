aadbadb22eef6a98ff9f1cc2a6a66e99d3d6b150
Tarik Yayla <tarik.yayla@ouriginal.com>
2022-07-10 22:19:25 +0200

    removed strict slashes, added a basic validator and errorhandler for invalid query params

--- api/actions.py
+++ api/actions.py
@@ -1,4 +1,6 @@
+import code
 from flask import Blueprint, jsonify, request
+import werkzeug
 from models import Locations 
 from playhouse.shortcuts import model_to_dict
 from flask_cors import CORS
@@ -9,6 +11,15 @@ CORS(api)
 # I didnt want to install pydantic for validation
  # so thats why its pretty easy to break if you insist. 
 
+class InvalidQueryParamException(Exception): 
+  pass
+
+
+@api.errorhandler(InvalidQueryParamException)
+def invalid_query_param_handler(e):
+  return {"ex": "Query param is not valid"}, 400
+
+
 class QueryParams(object):
   page = 0
   items_per_page = 20
@@ -17,10 +28,25 @@ class QueryParams(object):
     self.page = page
     self.items_per_page = items_per_page
 
+  @staticmethod 
+  def is_valid_int(value):
+    """ too lazy to implement something proper, should work"""
+    try:
+      value = int(value)
+      if not isinstance(value, int) or value < 0: raise Exception
+    except: 
+      raise InvalidQueryParamException
+
+    return True
+
+
   @staticmethod
   def get_from_args(args):
     page = int(args.get("page", 0))
     items_per_page = int(args.get("items-per-page", 20))
+
+    QueryParams.is_valid_int(page)
+    QueryParams.is_valid_int(items_per_page)
 
     return QueryParams(page, items_per_page)
 
@@ -35,6 +61,7 @@ def get_locations():
 
 @api.route("locations/<id>", methods=["GET"])
 def get_location(id: int): 
+  QueryParams.is_valid_int(id) 
   location = Locations.get_or_none(Locations.id == id) 
 
   if location is None:
--- api/app.py
+++ api/app.py
@@ -3,5 +3,6 @@ from config import Config
 from flask_peewee.db import Database
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 app.config.from_object(Config)
 db = Database(app)
